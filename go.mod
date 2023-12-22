e7b40493d44ddb089fe0e93b560978648b2d6bda
Bornium Sistema <bornium@bornium.com.br>
2023-12-21 22:17:33 -0300

    Ajuste

---
+++ go.mod
@@ -0,0 +1,3 @@
+module teste02
+
+go 1.21.5
---
+++ teste.go
@@ -0,0 +1,17 @@
+package teste02
+
+type Tabc struct {
+	a1 int64
+	b2 string
+}
+
+func PauloCesar() Tabc {
+
+	var abc Tabc
+
+	abc.a1 = 1
+	abc.b2 = "aaa"
+
+	return abc
+
+}
